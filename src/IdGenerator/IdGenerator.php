6e2717598632c3c96388340308fa85186cf5bc5c
Gabriel Felipe <email@gabrielfelipe.com>
2017-10-07 17:20:27 -0300

    Machine id wasn't being concatenated to id

--- src/IdGenerator/IdGenerator.php
+++ src/IdGenerator/IdGenerator.php
@@ -19,7 +19,7 @@ class IdGenerator
 
     private static $counter = 0;
     private static $lastMs = false;
-    private static $machineId = 00;
+    private static $machineId = "00";
     private static $counterLimit = 9999;
     public static function getId()
     {
@@ -41,7 +41,7 @@ class IdGenerator
         }
 
         $counter = self::getCounter($time);
-        return $time.$counter;
+        return $time.self::$machineId.$counter;
     }
 
     private static function getTimestamp()
